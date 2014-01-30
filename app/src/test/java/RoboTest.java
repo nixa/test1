fe24ab3a5ce6523ef712208effd5bb98aa8dbc53
Nikola Kapraljevic nixa <nikola@infinum.hr>
2014-01-30 16:33:37 +0100

    Robolectric

---
+++ app/src/test/java/RoboTest.java
@@ -0,0 +1,28 @@
+import android.app.Activity;
+import android.widget.TextView;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import org.robolectric.*;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.junit.Assert.assertThat;
+
+@RunWith(RobolectricTestRunner.class)
+public class RoboTest {
+
+    @Test
+    public void shouldUpdateResultsWhenButtonIsClicked() throws Exception {
+        String testText = "Hello World!";
+        TextView view = new TextView(Robolectric.application);
+
+        view.setText(testText);
+        String viewText = view.getText().toString();
+
+        assertThat(viewText, equalTo(testText));
+    }
+
+
+}
